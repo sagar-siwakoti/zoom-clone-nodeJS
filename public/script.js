7dbb9975a033242ea4ee3d4cd110eab41f56d5ed
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-09-17 12:20:11 +0545

    -added mute unmute features

--- public/script.js
+++ public/script.js
@@ -67,5 +67,38 @@ $("html").keydown((e) => {
 
 socket.on("createMessage", message => {
   $("ul").append(`<li class="message"><b>user</b><br/>${message}</li>`);
-
+  scroolToBottom();
 })
+
+const scroolToBottom=()=>{
+  let d =$(".main__chat_window");
+  d.scrollTop(d.prop("scroolHeight"));
+}
+//mute and unmute
+const muteUnmute = () => {
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    setUnmuteButton();
+  } else {
+    setMuteButton();
+    myVideoStream.getAudioTracks()[0].enabled = true;
+  }
+}
+
+
+const setMuteButton = () => {
+  const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>
+  `
+  document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setUnmuteButton = () => {
+  const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+  `
+  document.querySelector('.main__mute_button').innerHTML = html;
+}
